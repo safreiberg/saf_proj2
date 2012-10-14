14b1e54209013ba0f0288091ea4db737e2c09da3
Stephen Freiberg <safreiberg@gmail.com>
2012-10-14 19:52:39 -0400

    working

--- app/controllers/cart_controller.rb
+++ app/controllers/cart_controller.rb
@@ -28,6 +28,14 @@ class CartController < ApplicationController
       params.each do |key, value| 
         if (key.to_s[/.*quantity/])
           po = ProductOrder.where(:product_id => key.to_s.chomp(".quantity"), :cart_id => session[:cart].id).first
+          if value > Product.find_by_id(self.product_id).inventory
+            value = Product.find_by_id(self.product_id).inventory
+            flash[:notice] = "You attempted to purchase more than the inventory, so we decreased your order to the max."
+          end
+          if value < 0
+            value = 1
+            flash[:notice] = "You must buy at least one! Silly goose."
+          end
           po.change_quantity(value)
         end
       end
